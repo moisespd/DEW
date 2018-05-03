ea56a9a9d4ee47f0c2febcb8a26e0d361a4ff0e7
Moisés <moises@whatever.com>
2018-05-03 17:14:55 +0100

    _

--- v7/my_ajax_lib.js
+++ v7/my_ajax_lib.js
@@ -4,7 +4,14 @@ var remoteURI = "https://moisespd.github.io/DEW/v7/server/";
 function ajaxRETR_connected_players() {
     if (this.readyState == 4 && this.status == 200) {
         var connected_players = JSON.parse(this.responseText);
-        document.getElementById("connected_players").innerHTML = connected_players.length;
+        var div_connected_players = document.getElementById("connected_players");
+
+        div_connected_players.innerHTML = connected_players.length + " players connected";
+        div_connected_players.innerHTML += "<ul>";
+        for (var i = 0; i < connected_players.length; i++) {
+          div_connected_players.innerHTML += "<li>" + connected_players[i].name + "</li>";
+        }
+        div_connected_players.innerHTML += "</ul>";
     }
 }
 // --------------------------------------------------------------------
